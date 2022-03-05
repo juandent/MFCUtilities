c036d8afd3b2aaa68c401592318565bb9ecc2246
JUAN DENT <juandent@mac.com>
2022-03-05 11:27:29 -0600

    added SQLCookbook

--- sqlite_orm_examples/SQLCookbook.cpp
+++ sqlite_orm_examples/SQLCookbook.cpp
@@ -16,7 +16,40 @@ struct Employee
 	int m_depno;
 };
 
-using namespace sqlite_orm;
+struct Department
+{
+	int m_deptno;
+	std::string m_deptname;
+	std::string m_loc;
 
-auto& storage = make_storage("SQLCookbook",
-								make_table("Emp", make_column("empno", &Employee::m_empno)))
+};
+
+
+
+int main()
+{
+	using namespace sqlite_orm;
+
+	static auto storage = make_storage("SQLCookbook.sqlite",
+		make_table("Emp", 
+			make_column("empno", &Employee::m_empno, primary_key(), autoincrement()),
+			make_column("ename", &Employee::m_ename),
+			make_column("job", &Employee::m_job),
+			make_column("mgr", &Employee::m_mgr),
+			make_column("hiredate", &Employee::m_hiredate),
+			make_column("salary", &Employee::m_salary),
+			make_column("comm", &Employee::m_commission),
+			make_column("depno", &Employee::m_depno),
+			foreign_key(&Employee::m_depno).references(&Department::m_deptno)),
+		make_table("Dep",
+			make_column("deptno", &Department::m_deptno, primary_key(), autoincrement()),
+			make_column("deptname", &Department::m_deptname),
+			make_column("loc", &Department::m_loc)));
+
+	storage.sync_schema();
+
+	std::vector<Employee> vec = {}
+
+
+	storage.replace()
+}
